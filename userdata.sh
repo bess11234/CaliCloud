5caf1bbf1e9645df95077b1ff985cad5f44f3344
kasomsri <bess11234@hotmail.com>
2024-11-02 11:14:53 +0700

    INSTALL web-token/jwt

--- userdata.sh
+++ userdata.sh
@@ -4,6 +4,7 @@ sudo rm -rf ~/app
 sudo git clone https://github.com/bess11234/CaliCloud.git app
 cd app
 sudo yum install -y nginx
+sudo yum install -y php-xml
 sudo cp nginx.conf /etc/nginx/
 sudo mkdir /etc/pki/nginx
 sudo cp ssl/calicloud.crt /etc/pki/nginx/
@@ -17,7 +18,7 @@ sudo yum install -y php-cli php-zip wget unzip
 sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 sudo npm install
-sudo composer install
+sudo php /usr/local/bin/composer install
 sudo npm run build
 sudo cp -r dist/* /usr/share/nginx/html/
 php -S localhost:8000
