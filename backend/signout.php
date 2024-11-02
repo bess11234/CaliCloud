29d50f33785c2af2eb3feee673e2deda95328031
kasomsri <bess11234@hotmail.com>
2024-11-02 17:29:21 +0700

    FIXED signout

--- backend/signout.php
+++ backend/signout.php
@@ -4,5 +4,4 @@ session_start();
 session_destroy();
 
 header("HTTP/1.1 200 OK");
-header("location: https://calicloudgooglev2.auth.us-east-1.amazoncognito.com/logout?client_id=7uvm35k22tckpfkhbq46b7jkut&redirect_uri=https%3A%2F%2F3.82.155.205%2Fbackend%2Ftest.php&response_type=code")
 ?>
