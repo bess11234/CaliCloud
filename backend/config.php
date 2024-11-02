4a6aa943c07f0f5d4fe28e8b298132fd462e429a
kasomsri <bess11234@hotmail.com>
2024-11-02 15:19:23 +0700

    COMPLETE credential

--- backend/config.php
+++ backend/config.php
@@ -7,8 +7,10 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 // Allow specific headers, including Content-Type
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
 
 define('AWS_REGION', 'us-east-1');
 define('USER_POOL_ID', 'us-east-1_IJfiebLkV');
 define('CLIENT_ID', '7uvm35k22tckpfkhbq46b7jkut');
+define("CLIENT_SECRET", "1dc44dl0ae6f0823t84elteejcvl0tlhkve3uigb7u86a0i8hbfa");
 ?>
