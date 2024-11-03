0a753f4b8222391e506dc1ce5cd556abbfea1403
kasomsri <bess11234@hotmail.com>
2024-11-03 22:13:25 +0700

    CHANGE directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./*.html', './src/**/*.{js,ts,jsx,tsx,html}'],
+  content: ['./*.html', './page/**/*.{js,ts,jsx,tsx,html}'],
   theme: {
     extend: {
       colors:{
